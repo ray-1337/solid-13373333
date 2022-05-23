0e4f2dcdb7583c0d6bcc83e98085cb0b29af94e7
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-05-23 23:55:01 +0800

    added content-security-policy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import * as Util from "./src/Util";
 
 import solidPlugin from 'vite-plugin-solid';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import viteCSP from "vite-plugin-csp";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,26 @@ export default defineConfig({
     createHtmlPlugin({
       minify: true,
       template: "/index.html"
+    }),
+
+    viteCSP({
+      policy: {
+        "default-src": "self",
+        "script-src": "self",
+        "frame-src": ["none"],
+        "img-src": ["self"],
+        "connect-src": ["self"],
+        "font-src": [
+          "self",
+          "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/",
+          "https://fonts.gstatic.com/s/",
+        ],
+        "style-src": [
+          "self",
+          "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/",
+          "https://fonts.googleapis.com/"
+        ]
+      }
     })
   ],
   
