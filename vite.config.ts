a1295aff7fb742b66714d0414544e105f520f428
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-05-23 23:23:44 +0800

    modified rollup build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import * as Util from "./src/Util"
 
 export default defineConfig({
   plugins: [solidPlugin()],
   
   build: {
+    cssCodeSplit: false,
+    minify: true,
     target: 'esnext',
     polyfillDynamicImport: false,
-  },
+    rollupOptions: {
+      output: {
+        assetFileNames: () => {
+          return "firstclass_upinthesky/" + Util.generateString(32) + "[extname]";
+        },
+
+        chunkFileNames: () => {
+          return "firstclass_upinthesky/" + Util.generateString(32) + ".js";
+        },
+
+        entryFileNames: () => {
+          return "firstclass_upinthesky/" + Util.generateString(32) + ".js";
+        }
+      }
+    }
+  }
 });
