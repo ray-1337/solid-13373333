dca2805fe8982ab010cce7210ca2eb0806f59b39
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-07-01 12:21:50 +0800

    added visit button on jobs panel

--- src/components/Bio/Occupation.tsx
+++ src/components/Bio/Occupation.tsx
@@ -13,6 +13,11 @@ const Bio_Occupation: Component = () => {
 
   let leftArrow!: HTMLSpanElement;
   let rightArrow!: HTMLSpanElement;
+  let currentVisitURI = "";
+
+  function setVisitButton(index: number) {
+    return List()[index].url ? currentVisitURI = List()[index].url : void(0);
+  };
 
   function leftBtn() {
     let index = occupationList.findIndex(val => val.classList.contains(style.ocu_Open));
@@ -26,6 +31,8 @@ const Bio_Occupation: Component = () => {
     if (calculation < 0) {
       return;
     };
+
+    setVisitButton(calculation);
 
     occupationList[index].classList.remove(style.ocu_Open);
     occupationList[calculation].classList.add(style.ocu_Open);
@@ -41,11 +48,12 @@ const Bio_Occupation: Component = () => {
     };
 
     let calculation = index + 1;
-    console.log(calculation)
     if (index >= occupationList.length - 2) rightArrow.classList.add(style.btnStop);
     if (calculation >= occupationList.length) {
       return;
     };
+
+    setVisitButton(calculation);
 
     occupationList[index].classList.remove(style.ocu_Open);
     occupationList[calculation].classList.add(style.ocu_Open);
@@ -54,10 +62,10 @@ const Bio_Occupation: Component = () => {
   };
 
   onMount(() => {
-    console.log(occupationList)
     if (occupationList[0]) {
       occupationList[0].classList.add(style.ocu_Open);
       leftArrow.classList.add(style.btnStop);
+      setVisitButton(0);
     };
   });
 
@@ -95,13 +103,20 @@ const Bio_Occupation: Component = () => {
       <div class={style.occupationSubmissive_button}>
         {/* https://iconmonstr.com/ */}
 
-        <span class={style.btnLeft} ref={(evt) => leftArrow = evt} onclick={() => leftBtn()}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" /></svg>
-        </span>
+        <div class={style.btnRedirect} onclick={() => currentVisitURI.length ? window.open(currentVisitURI, "_blank") : void(0)}>
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>
+          <h1>Visit</h1>
+        </div>
 
-        <span class={style.btnRight} ref={(evt) => rightArrow = evt} onclick={() => rightBtn()}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg>
-        </span>
+        <div class={style.btnPanel}>
+          <span class={style.btnLeft} ref={(evt) => leftArrow = evt} onclick={() => leftBtn()}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" /></svg>
+          </span>
+
+          <span class={style.btnRight} ref={(evt) => rightArrow = evt} onclick={() => rightBtn()}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z" /></svg>
+          </span>
+        </div>
 
       </div>
     </div>
