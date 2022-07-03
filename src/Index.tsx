e79bb9f710e29dac2aac72a9bc01656b6fb69db6
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-07-03 21:53:46 +0800

    added meta tags

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,6 +1,9 @@
 import { Component, lazy, onCleanup, onMount } from 'solid-js';
+import { MetaProvider, Meta } from "solid-meta";
 
 import Menu from './components/Menu';
+
+import personalImage from "./assets/images/personal_content/IMG_20220703_195320.webp";
 
 const Bio = lazy(() => import("./components/Bio/Index"));
 const Social = lazy(() => import('./components/Social'));
@@ -22,15 +25,27 @@ const Index: Component = () => {
   })
 
   return (
-    <>
-      <Menu></Menu>
+    <MetaProvider>
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta name="twitter:site" content="@ray__1337" />
+      <Meta name="twitter:creator" content="@ray__1337" />
+      <Meta name="theme-color" content="#f7a937"/>
+      <Meta property="og:type" content="website" />
+      <Meta property="og:title" content="13373333.one" />
+      <Meta property="og:description" content="In his right hand he held seven stars, and coming out of his mouth was a sharp, double-edged sword. His face was like the sun shining in all its brilliance."/>
+      <Meta property="twitter:image" content={personalImage}/>
+      <Meta property="og:image" content={personalImage}/>
 
-      <Bio></Bio>
+      <>
+        <Menu></Menu>
 
-      <Social></Social>
+        <Bio></Bio>
 
-      <Project></Project>
-    </>
+        <Social></Social>
+
+        <Project></Project>
+      </>
+    </MetaProvider>
   );
 };
 
