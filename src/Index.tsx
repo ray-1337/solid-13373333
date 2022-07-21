38950df59ab31025c19acca1fe32bd6971351695
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-07-21 16:22:58 +0800

    load font from MetaProvider

--- src/Index.tsx
+++ src/Index.tsx
@@ -39,6 +39,8 @@ const Index: Component = () => {
       <Meta property="twitter:image" content={personalImage}/>
       <Meta property="og:image" content={personalImage}/> */}
       <Link href={favicon} rel="shortcut icon"/>
+      <Link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="preload stylesheet"/>
+      <Link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="preload stylesheet"/>
 
       <>
         <Menu></Menu>
