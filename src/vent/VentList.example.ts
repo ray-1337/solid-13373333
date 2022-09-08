11e9fd2d187b375a3e36e875a26aafd6f8bf797d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2022-09-09 01:29:01 +0800

    ventlist example revision

--- src/vent/VentList.example.ts
+++ src/vent/VentList.example.ts
@@ -1,9 +1,16 @@
-import { html } from "common-tags";
+import { marked } from "marked";
 
 interface VentComponents {
   date: Date;
   content: string;
+  images?: string;
 }
+
+function parseContent(content: string) {
+  return marked.parseInline(content, {
+    gfm: true, breaks: true
+  });
+};
 
 const Things: Array<VentComponents> = [
 
